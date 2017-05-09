08fed7d99c7f6f551ade5f7fa61b0aae294d041e
Chris Milson <chris.w.milson@gmail.com>
2017-05-09 22:25:58 +1200

    Fix location of output

--- install-tools.py
+++ install-tools.py
@@ -1,7 +1,7 @@
 import sys, os
 from subprocess import call
 
-filename = "../new"
+filename = "./new"
 
 tool = open(filename, "w") # not worried whether it exists or not.
 
@@ -9,4 +9,4 @@ tool.write("#!/bin/bash\n")
 
 tool.write("python devtools/setup-main.py \"$@\"")
 
-call(["chmod", "+x", "../new"])
+call(["chmod", "+x", "./new"])
